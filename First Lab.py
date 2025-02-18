b9ba34bf846499858167512fa8ef4071c1fc4064
labzhz <zhz19960406@qq.com>
2025-02-18 17:30:21 +1300

    Create First Lab.py

---
+++ First Lab.py
@@ -0,0 +1,66 @@
+# import math
+# print('hello lab')
+# print(math.sqrt(2))
+# print(10000-200.3-((10000-200.3)//400)*50)
+# goodWeather=True
+# if goodWeather==True:
+#     print('play basketball')
+# else :
+#     print('laying')
+# weather='snow'
+# if weather=='snow':
+#     print('cold')
+# elif weather=='rain':
+#     print('wet')
+# elif weather=='wind'or weather=='cloudy':
+#     print('cool')
+# else :
+#     print('good day')
+# count=0
+# while count<10:
+#     print('I love you')
+#     count+=2
+# SUM =0
+# adder=1
+# while adder<=100:
+#    SUM+=adder
+#    adder+=1
+# print(SUM)
+# number=1
+# while number<=100:
+#     if not number%2==0:
+#         print(number)
+#     number+=1
+# number=5
+# result=1
+# count=1
+# while count<=number:
+#     result=result*count
+#     count+=1
+# print(result)
+# sum = 0
+# number = 1
+# while number <= 4:
+#     result = 1
+#     count = 1
+#     while count <= number:
+#         result *= count
+#         count+=1
+#     if number % 2 == 0:
+#         sum -= result
+#     else:
+#         sum += result
+#     number+=1
+# print(sum)
+# number=2
+# while number<=100:
+#     count=2
+#     prime=True
+#     while count<number:
+#         if number%count==0:
+#             prime=False
+#             break
+#         count+=1
+#     if prime:
+#         print(number)
+#     number+=1
